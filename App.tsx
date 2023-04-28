8e03a0c02312bdc683caa96f0c40034954018a70
shtjdqhd <shtjdqhd@naver.com>
2023-04-28 15:36:36 +0900

    test:8

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import {StyleSheet, Text, View} from 'react-native/types';
 function App(): JSX.Element {
   return (
     <View>
-      <Text>tes5</Text>
+      <Text>tes6</Text>
     </View>
   );
 }
